1b6720ed25bdd20fbf745bc24317eda06b0274fa
pwzCypher <phyowaizaw.aidma@gmail.com>
2015-10-30 10:28:07 +0000

    searchadded

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,37 @@
 				$(".spmenu").slideToggle("slow");
 			});
 		});
+		
+		
+		// ■画像ロールオーバー
+		// http://rewish.org/javascript/jquery_rollover_plugin
+		jQuery.fn.rollover = function(suffix) {
+			suffix = suffix || '_on';
+			var check = new RegExp(suffix + '\\.\\w+$');
+			return this.each(function() {
+				var img = jQuery(this);
+				var src = img.attr('src');
+				if (check.test(src)) return;
+				var _on = src.replace(/\.\w+$/, suffix + '$&');
+				jQuery('<img>').attr('src', _on);
+				img.hover(
+					function() { img.attr('src', _on); },
+					function() { img.attr('src', src); }
+				);
+			});
+		};
+
+		// ■スムーズスクロール
+		// http://www.daichifive.com/
+		(function(jQuery) {
+			jQuery.fn.scRoller = function() {
+				return this.each(function() {
+					jQuery(this).click(function() {
+						jQuery('html,body').animate({
+							scrollTop: jQuery(jQuery(this).attr("href")).offset().top
+						});
+						return false;
+					});
+				});
+			};
+		})(jQuery);
